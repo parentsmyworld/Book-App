db99045e4e9f2403216624440bf6609e19a5ddd5
parentsmyworld <madhu@skylinkmsc.com>
2020-08-16 23:49:40 +0300

    debugandscroll

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Center(child: Text("BOOK APP")),
         ),
-        body:Container(
-          child: Column(
-            children: [
-              Text("Title"),
+        body:SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                Text("Title"),
+                TextField(),
+              Text("Author"),
               TextField(),
-            Text("Author"),
-            TextField(),
-              Text("Description"),
-              TextField(),
-              Text("Price"),
-              TextField(),
-              RaisedButton(
-                child: Text("SUBMIT"),
-              )
+                Text("Description"),
+                TextField(),
+                Text("Price"),
+                TextField(),
+                RaisedButton(
+                  child: Text("SUBMIT"),
+                )
 
-            ],
+              ],
+            ),
           ),
         )
       ),
